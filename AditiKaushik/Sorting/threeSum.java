6a21c8ecba7550470f6eb32054d3b3f3ae4c6653
Aditi-Kaushik04 <aditikaushik6279.11e@gmail.com>
2025-02-25 15:00:03 +0530

    Adding sorting question

---
+++ AditiKaushik/Sorting/threeSum.java
@@ -0,0 +1,52 @@
+// 15. 3Sum
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+class threeSum{
+    public static List<List<Integer>> three_Sum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
+        int n = nums.length;
+        for (int i = 0; i < n - 2 && nums[i] <= 0; ++i) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int j = i + 1, k = n - 1;
+            while (j < k) {
+                int x = nums[i] + nums[j] + nums[k];
+                if (x < 0) {
+                    ++j;
+                } else if (x > 0) {
+                    --k;
+                } else {
+                    ans.add(List.of(nums[i], nums[j++], nums[k--]));
+                    while (j < k && nums[j] == nums[j - 1]) {
+                        ++j;
+                    }
+                    while (j < k && nums[k] == nums[k + 1]) {
+                        --k;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of an array: ");
+        int size = sc.nextInt();
+        int[] nums = new int[size];
+        for(int i =0; i< size; i++){
+            System.out.println("Enter element " + (i+1) + " : ");
+            nums[i] = sc.nextInt();
+        }
+        System.out.println(three_Sum(nums));
+        sc.close();
+    }
+}
+
+//Time Complexity: O(n^2)
+//Space Complexity: O(n^2)
